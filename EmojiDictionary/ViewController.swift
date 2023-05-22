198b9f898a2538d1d48fe79962344bf819af96b2
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-22 19:16:22 -0400

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Jon Salkin on 5/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
