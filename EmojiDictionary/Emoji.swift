6f37379154057443c1a37a705f042accad64332f
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-22 20:28:01 -0400

    Initial commit.

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Jon Salkin on 5/22/23.
+//
+
+import Foundation
+
+struct Emoji {
+    let symbol: String
+    let name: String
+    let description: String
+    let usage: String
+}
+
